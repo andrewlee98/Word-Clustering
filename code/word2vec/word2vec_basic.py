43d706cf97812475e5623c76f6853162b006db6b
Andrew Lee <andrew.warren.lee@gmail.com>
2017-06-16 21:17:37 -0700

    Fixed tabulation and spacing.

--- code/word2vec/word2vec_basic.py
+++ code/word2vec/word2vec_basic.py
@@ -50,42 +50,42 @@ def flatten(l):
 
 # Read the data into a list of strings.
 def read_data(filename):
-  with open(filename) as f:
-    data = [line.rstrip('\n') for line in open(filename)]
-  data = [word_tokenize(datum) for datum in data]
+    with open(filename) as f:
+        data = [line.rstrip('\n') for line in open(filename)]
+    data = [word_tokenize(datum) for datum in data]
 
-  regex = re.compile('[%s]' % re.escape(string.punctuation))
-  tokenized_docs_no_punctuation = []
-  for review in data:
-      new_review = []
-      for token in review:
-          new_token = regex.sub(u'', token)
-          if not new_token == u'':
-              new_review.append(new_token)
-      tokenized_docs_no_punctuation.append(new_review)
+    regex = re.compile('[%s]' % re.escape(string.punctuation))
+    tokenized_docs_no_punctuation = []
+    for review in data:
+        new_review = []
+        for token in review:
+            new_token = regex.sub(u'', token)
+            if not new_token == u'':
+                new_review.append(new_token)
+        tokenized_docs_no_punctuation.append(new_review)
 
-  # stem
-  porter = PorterStemmer()
-  docs_stemmed = []
-  for doc in tokenized_docs_no_punctuation:
-    final_doc = []
-    for word in doc:
-      final_doc.append(porter.stem(word))
-    docs_stemmed.append(final_doc)
+    # stem
+    porter = PorterStemmer()
+    docs_stemmed = []
+    for doc in tokenized_docs_no_punctuation:
+        final_doc = []
+        for word in doc:
+            final_doc.append(porter.stem(word))
+        docs_stemmed.append(final_doc)
 
-  # flatten into one dimensional list
-  data = flatten(docs_stemmed)
-  data = [x.lower() for x in data]
+    # flatten into one dimensional list
+    data = flatten(docs_stemmed)
+    data = [x.lower() for x in data]
 
-  data = [word for word in data if word not in stopwords.words('english')]
+    data = [word for word in data if word not in stopwords.words('english')]
 
 
-  # clean digits
-  for item in data[:]:
-      if any(char.isdigit() for char in item) or len(item)<2:
-          data.remove(item)
+    # clean digits
+    for item in data[:]:
+        if any(char.isdigit() for char in item) or len(item)<2:
+            data.remove(item)
 
-  return data
+    return data
 
 
 words = read_data(filename)
@@ -96,23 +96,23 @@ print('Data size', len(words))
 vocabulary_size = 1000
 
 def build_dataset(words, vocabulary_size):
-  count = [['UNK', -1]]
-  count.extend(collections.Counter(words).most_common(vocabulary_size - 1))
-  dictionary = dict()
-  for word, _ in count:
-    dictionary[word] = len(dictionary)
-  data = list()
-  unk_count = 0
-  for word in words:
-    if word in dictionary:
-      index = dictionary[word]
-    else:
-      index = 0  # dictionary['UNK']
-      unk_count += 1
-    data.append(index)
-  count[0][1] = unk_count
-  reverse_dictionary = dict(zip(dictionary.values(), dictionary.keys()))
-  return data, count, dictionary, reverse_dictionary
+    count = [['UNK', -1]]
+    count.extend(collections.Counter(words).most_common(vocabulary_size - 1))
+    dictionary = dict()
+    for word, _ in count:
+        dictionary[word] = len(dictionary)
+    data = list()
+    unk_count = 0
+    for word in words:
+        if word in dictionary:
+            index = dictionary[word]
+        else:
+            index = 0  # dictionary['UNK']
+            unk_count += 1
+        data.append(index)
+    count[0][1] = unk_count
+    reverse_dictionary = dict(zip(dictionary.values(), dictionary.keys()))
+    return data, count, dictionary, reverse_dictionary
 
 data, count, dictionary, reverse_dictionary = \
 build_dataset(words, vocabulary_size)
@@ -125,35 +125,35 @@ data_index = 0
 
 # Step 3: Function to generate a training batch for the skip-gram model.
 def generate_batch(batch_size, num_skips, skip_window):
-  global data_index
-  assert batch_size % num_skips == 0
-  assert num_skips <= 2 * skip_window
-  batch = np.ndarray(shape=(batch_size), dtype=np.int32)
-  labels = np.ndarray(shape=(batch_size, 1), dtype=np.int32)
-  span = 2 * skip_window + 1  # [ skip_window target skip_window ]
-  buffer = collections.deque(maxlen=span)
-  for _ in range(span):
-    buffer.append(data[data_index])
-    data_index = (data_index + 1) % len(data)
-  for i in range(batch_size // num_skips):
-    target = skip_window  # target label at the center of the buffer
-    targets_to_avoid = [skip_window]
-    for j in range(num_skips):
-      while target in targets_to_avoid:
-        target = random.randint(0, span - 1)
-      targets_to_avoid.append(target)
-      batch[i * num_skips + j] = buffer[skip_window]
-      labels[i * num_skips + j, 0] = buffer[target]
-    buffer.append(data[data_index])
-    data_index = (data_index + 1) % len(data)
-  # Backtrack a little bit to avoid skipping words in the end of a batch
-  data_index = (data_index + len(data) - span) % len(data)
-  return batch, labels
+    global data_index
+    assert batch_size % num_skips == 0
+    assert num_skips <= 2 * skip_window
+    batch = np.ndarray(shape=(batch_size), dtype=np.int32)
+    labels = np.ndarray(shape=(batch_size, 1), dtype=np.int32)
+    span = 2 * skip_window + 1  # [ skip_window target skip_window ]
+    buffer = collections.deque(maxlen=span)
+    for _ in range(span):
+        buffer.append(data[data_index])
+        data_index = (data_index + 1) % len(data)
+    for i in range(batch_size // num_skips):
+        target = skip_window  # target label at the center of the buffer
+        targets_to_avoid = [skip_window]
+        for j in range(num_skips):
+            while target in targets_to_avoid:
+                target = random.randint(0, span - 1)
+            targets_to_avoid.append(target)
+            batch[i * num_skips + j] = buffer[skip_window]
+            labels[i * num_skips + j, 0] = buffer[target]
+        buffer.append(data[data_index])
+        data_index = (data_index + 1) % len(data)
+    # Backtrack a little bit to avoid skipping words in the end of a batch
+    data_index = (data_index + len(data) - span) % len(data)
+    return batch, labels
 
 batch, labels = generate_batch(batch_size=8, num_skips=2, skip_window=1)
 for i in range(8):
-  print(batch[i], reverse_dictionary[batch[i]],
-        '->', labels[i, 0], reverse_dictionary[labels[i, 0]])
+    print(batch[i], reverse_dictionary[batch[i]],
+          '->', labels[i, 0], reverse_dictionary[labels[i, 0]])
 
 # Step 4: Build and train a skip-gram model.
 
@@ -174,139 +174,139 @@ graph = tf.Graph()
 
 with graph.as_default():
 
-  # Input data.
-  train_inputs = tf.placeholder(tf.int32, shape=[batch_size])
-  train_labels = tf.placeholder(tf.int32, shape=[batch_size, 1])
-  valid_dataset = tf.constant(valid_examples, dtype=tf.int32)
+    # Input data.
+    train_inputs = tf.placeholder(tf.int32, shape=[batch_size])
+    train_labels = tf.placeholder(tf.int32, shape=[batch_size, 1])
+    valid_dataset = tf.constant(valid_examples, dtype=tf.int32)
 
-  # Ops and variables pinned to the CPU because of missing GPU implementation
-  with tf.device('/cpu:0'):
-    # Look up embeddings for inputs.
-    embeddings = tf.Variable(
-        tf.random_uniform([vocabulary_size, embedding_size], -1.0, 1.0))
-    embed = tf.nn.embedding_lookup(embeddings, train_inputs)
+    # Ops and variables pinned to the CPU because of missing GPU implementation
+    with tf.device('/cpu:0'):
+        # Look up embeddings for inputs.
+        embeddings = tf.Variable(
+            tf.random_uniform([vocabulary_size, embedding_size], -1.0, 1.0))
+        embed = tf.nn.embedding_lookup(embeddings, train_inputs)
 
-    # Construct the variables for the NCE loss
-    nce_weights = tf.Variable(
-        tf.truncated_normal([vocabulary_size, embedding_size],
-                            stddev=1.0 / math.sqrt(embedding_size)))
-    nce_biases = tf.Variable(tf.zeros([vocabulary_size]))
+        # Construct the variables for the NCE loss
+        nce_weights = tf.Variable(
+            tf.truncated_normal([vocabulary_size, embedding_size],
+                                stddev=1.0 / math.sqrt(embedding_size)))
+        nce_biases = tf.Variable(tf.zeros([vocabulary_size]))
 
-  # Compute the average NCE loss for the batch.
-  # tf.nce_loss automatically draws a new sample of the negative labels each
-  # time we evaluate the loss.
-  loss = tf.reduce_mean(
-      tf.nn.nce_loss(weights=nce_weights,
-                     biases=nce_biases,
-                     labels=train_labels,
-                     inputs=embed,
-                     num_sampled=num_sampled,
-                     num_classes=vocabulary_size))
+    # Compute the average NCE loss for the batch.
+    # tf.nce_loss automatically draws a new sample of the negative labels each
+    # time we evaluate the loss.
+    loss = tf.reduce_mean(
+            tf.nn.nce_loss(weights=nce_weights,
+                           biases=nce_biases,
+                           labels=train_labels,
+                           inputs=embed,
+                           num_sampled=num_sampled,
+                           num_classes=vocabulary_size))
 
-  # Construct the SGD optimizer using a learning rate of 1.0.
-  optimizer = tf.train.GradientDescentOptimizer(1.0).minimize(loss)
+    # Construct the SGD optimizer using a learning rate of 1.0.
+    optimizer = tf.train.GradientDescentOptimizer(1.0).minimize(loss)
 
-  # Compute the cosine similarity between minibatch examples and all embeddings.
-  norm = tf.sqrt(tf.reduce_sum(tf.square(embeddings), 1, keep_dims=True))
-  normalized_embeddings = embeddings / norm
-  valid_embeddings = tf.nn.embedding_lookup(
-      normalized_embeddings, valid_dataset)
-  similarity = tf.matmul(
-      valid_embeddings, normalized_embeddings, transpose_b=True)
+    # Compute the cosine similarity between minibatch examples and all embeddings.
+    norm = tf.sqrt(tf.reduce_sum(tf.square(embeddings), 1, keep_dims=True))
+    normalized_embeddings = embeddings / norm
+    valid_embeddings = tf.nn.embedding_lookup(
+            normalized_embeddings, valid_dataset)
+    similarity = tf.matmul(
+            valid_embeddings, normalized_embeddings, transpose_b=True)
 
-  # Add variable initializer.
-  init = tf.global_variables_initializer()
+    # Add variable initializer.
+    init = tf.global_variables_initializer()
 
 # Step 5: Begin training.
 num_steps = 50001 # 100001
 
 with tf.Session(graph=graph) as session:
-  # We must initialize all variables before we use them.
-  init.run()
-  print("Initialized")
+    # We must initialize all variables before we use them.
+    init.run()
+    print("Initialized")
 
-  average_loss = 0
-  for step in xrange(num_steps):
-    batch_inputs, batch_labels = generate_batch(
-        batch_size, num_skips, skip_window)
-    feed_dict = {train_inputs: batch_inputs, train_labels: batch_labels}
+    average_loss = 0
+    for step in xrange(num_steps):
+        batch_inputs, batch_labels = generate_batch(
+                batch_size, num_skips, skip_window)
+        feed_dict = {train_inputs: batch_inputs, train_labels: batch_labels}
 
-    # We perform one update step by evaluating the optimizer op (including it
-    # in the list of returned values for session.run()
-    _, loss_val = session.run([optimizer, loss], feed_dict=feed_dict)
-    average_loss += loss_val
+        # We perform one update step by evaluating the optimizer op (including it
+        # in the list of returned values for session.run()
+        _, loss_val = session.run([optimizer, loss], feed_dict=feed_dict)
+        average_loss += loss_val
 
-    if step % 2000 == 0:
-      if step > 0:
-        average_loss /= 2000
-      # The average loss is an estimate of the loss over the last 2000 batches.
-      print("Average loss at step ", step, ": ", average_loss)
-      average_loss = 0
+        if step % 2000 == 0:
+            if step > 0:
+                average_loss /= 2000
+            # The average loss is an estimate of the loss over the last 2000 batches.
+            print("Average loss at step ", step, ": ", average_loss)
+            average_loss = 0
 
-    # Note that this is expensive (~20% slowdown if computed every 500 steps)
-    if step % 10000 == 0:
-      sim = similarity.eval()
-      for i in xrange(valid_size):
-        valid_word = reverse_dictionary[valid_examples[i]]
-        top_k = 8  # number of nearest neighbors
-        nearest = (-sim[i, :]).argsort()[1:top_k + 1]
-        log_str = "Nearest to %s:" % valid_word
-        for k in xrange(top_k):
-          close_word = reverse_dictionary[nearest[k]]
-          log_str = "%s %s," % (log_str, close_word)
-        print(log_str)
-  final_embeddings = normalized_embeddings.eval()
+        # Note that this is expensive (~20% slowdown if computed every 500 steps)
+        if step % 10000 == 0:
+            sim = similarity.eval()
+            for i in xrange(valid_size):
+                valid_word = reverse_dictionary[valid_examples[i]]
+                top_k = 8  # number of nearest neighbors
+                nearest = (-sim[i, :]).argsort()[1:top_k + 1]
+                log_str = "Nearest to %s:" % valid_word
+                for k in xrange(top_k):
+                    close_word = reverse_dictionary[nearest[k]]
+                    log_str = "%s %s," % (log_str, close_word)
+                print(log_str)
+    final_embeddings = normalized_embeddings.eval()
 
 # write embeddings to file
 with open('embeddings.txt','w+') as f:
-  for vec, word in zip(final_embeddings, reverse_dictionary):
-    f.write(reverse_dictionary[word] + ": " + \
-    str(["{0:0.2f}".format(i) for i in vec]) + "\n\n")
+    for vec, word in zip(final_embeddings, reverse_dictionary):
+        f.write(reverse_dictionary[word] + ": " + \
+        str(["{0:0.2f}".format(i) for i in vec]) + "\n\n")
 
 def cluster_func(vecs):
-  # normalize embeddings
-  vecs = StandardScaler().fit_transform(vecs)
+    # normalize embeddings
+    vecs = StandardScaler().fit_transform(vecs)
 
-  # dbscan
-  db = DBSCAN(eps=.3, min_samples=10).fit(vecs)
-  labels = db.labels_
-  n_clusters_ = len(set(labels)) - (1 if -1 in labels else 0)
-  print('Estimated number of clusters: %d' % n_clusters_)
-  if n_clusters_ < 1:
-    n_clusters_ = n_clusters_ + 1
-  cluster_lists = [[] for x in xrange(n_clusters_)]
-  with open("clusters.txt", "w+") as text_file:
-    text_file.write("=====DBSCAN (" + str(n_clusters_) + " clusters) :=====\n")
-    for x in range(0,n_clusters_):
-      for word_num in range(0, len(labels)):
-        if labels[word_num] == x-1:
-          cluster_lists[x].append(reverse_dictionary[word_num])
-      # write clusters to text file
-      text_file.write("cluster " + str(x) + ": " + str(cluster_lists[x]) + \
-      "\n\n")
+    # dbscan
+    db = DBSCAN(eps=.3, min_samples=10).fit(vecs)
+    labels = db.labels_
+    n_clusters_ = len(set(labels)) - (1 if -1 in labels else 0)
+    print('Estimated number of clusters: %d' % n_clusters_)
+    if n_clusters_ < 1:
+        n_clusters_ = n_clusters_ + 1
+    cluster_lists = [[] for x in xrange(n_clusters_)]
+    with open("clusters.txt", "w+") as text_file:
+        text_file.write("=====DBSCAN (" + str(n_clusters_) + " clusters) :=====\n")
+        for x in range(0,n_clusters_):
+            for word_num in range(0, len(labels)):
+                if labels[word_num] == x-1:
+                    cluster_lists[x].append(reverse_dictionary[word_num])
+            # write clusters to text file
+            text_file.write("cluster " + str(x) + ": " + str(cluster_lists[x]) + \
+            "\n\n")
 
-    text_file.write("\n\n\n")
+        text_file.write("\n\n\n")
 
-  #spectral
-  n_clusters = 10 #2-10
-  spectral = cluster.SpectralClustering(n_clusters,
+    #spectral
+    n_clusters = 10 #2-10
+    spectral = cluster.SpectralClustering(n_clusters,
                                           eigen_solver='arpack',
                                           affinity="nearest_neighbors")
-  spectral.fit(vecs);
-  y_pred = spectral.labels_.astype(np.int)
-  # create lists of clustered words
-  cluster_lists = [[] for x in xrange(n_clusters)]
-  with open("clusters.txt", "a") as text_file:
-    text_file.write("=====spectral:=====\n")
-    for x in range(0,n_clusters):
-      for word_num in range(0, len(y_pred)):
-        if y_pred[word_num] == x:
-          cluster_lists[x].append(reverse_dictionary[word_num])
-      # write clusters to text file
-      text_file.write("cluster #" + str(x + 1) + ": " + \
-      str(cluster_lists[x]) + "\n\n")
-  print("silhouette score: " + \
-  str(metrics.silhouette_score(vecs, y_pred, metric = 'sqeuclidean')))
+    spectral.fit(vecs);
+    y_pred = spectral.labels_.astype(np.int)
+    # create lists of clustered words
+    cluster_lists = [[] for x in xrange(n_clusters)]
+    with open("clusters.txt", "a") as text_file:
+        text_file.write("=====spectral:=====\n")
+        for x in range(0,n_clusters):
+            for word_num in range(0, len(y_pred)):
+                if y_pred[word_num] == x:
+                    cluster_lists[x].append(reverse_dictionary[word_num])
+            # write clusters to text file
+            text_file.write("cluster #" + str(x + 1) + ": " + \
+            str(cluster_lists[x]) + "\n\n")
+    print("silhouette score: " + \
+    str(metrics.silhouette_score(vecs, y_pred, metric = 'sqeuclidean')))
 
 cluster_func(final_embeddings)
 
@@ -327,29 +327,29 @@ cluster_func(final_embeddings)
 
 # Step 6: Visualize the embeddings.
 def plot_with_labels(low_dim_embs, labels, filename='tsne.png'):
-  assert low_dim_embs.shape[0] >= len(labels), "More labels than embeddings"
-  plt.figure(figsize=(18, 18))  # in inches
-  for i, label in enumerate(labels):
-    x, y = low_dim_embs[i, :]
-    plt.scatter(x, y)
-    plt.annotate(label,
-                 xy=(x, y),
-                 xytext=(5, 2),
-                 textcoords='offset points',
-                 ha='right',
-                 va='bottom')
+    assert low_dim_embs.shape[0] >= len(labels), "More labels than embeddings"
+    plt.figure(figsize=(18, 18))  # in inches
+    for i, label in enumerate(labels):
+        x, y = low_dim_embs[i, :]
+        plt.scatter(x, y)
+        plt.annotate(label,
+                     xy=(x, y),
+                     xytext=(5, 2),
+                     textcoords='offset points',
+                     ha='right',
+                     va='bottom')
 
-  plt.savefig(filename)
+    plt.savefig(filename)
 
 try:
-  from sklearn.manifold import TSNE
-  import matplotlib.pyplot as plt
+    from sklearn.manifold import TSNE
+    import matplotlib.pyplot as plt
 
-  tsne = TSNE(perplexity=30, n_components=2, init='pca', n_iter=5000)
-  plot_only = 500
-  low_dim_embs = tsne.fit_transform(final_embeddings[:plot_only, :])
-  labels = [reverse_dictionary[i] for i in xrange(plot_only)]
-  plot_with_labels(low_dim_embs, labels)
+    tsne = TSNE(perplexity=30, n_components=2, init='pca', n_iter=5000)
+    plot_only = 500
+    low_dim_embs = tsne.fit_transform(final_embeddings[:plot_only, :])
+    labels = [reverse_dictionary[i] for i in xrange(plot_only)]
+    plot_with_labels(low_dim_embs, labels)
 
 except ImportError:
-  print("Please install sklearn, matplotlib, and scipy to visualize embeddings.")
+    print("Please install sklearn, matplotlib, and scipy.")
