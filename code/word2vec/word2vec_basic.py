ec4ca9e844d0b447042199c136a6bce40e8cfdaa
Andrew Lee <andrew.warren.lee@gmail.com>
2017-06-17 21:32:40 -0700

    Started working on Davies-Bouldin index.

--- code/word2vec/word2vec_basic.py
+++ code/word2vec/word2vec_basic.py
@@ -264,6 +264,36 @@ with open('embeddings.txt','w+') as f:
         f.write(reverse_dictionary[word] + ": " + \
         str(["{0:0.2f}".format(i) for i in vec]) + "\n\n")
 
+def intra_cluster_dist(X, labels, cluster, num_items_in_cluster, centroid):
+    total_dist = 0
+    #for every item in cluster j, compute the distance the the center of cluster j, take average
+    for k in range(num_items_in_cluster):
+        dist = np.linalg.norm(X[labels==cluster]-centroid)
+        total_dist = dist + total_dist
+return total_dist/num_items_in_cluster
+
+def davies_bouldin(X, labels, cluster_ctr):
+    #get the cluster assignemnts
+    clusters = set(labels)
+    #get the number of clusters
+    num_clusters = len(clusters)
+    #array to hold the number of items for each cluster, indexed by cluster number
+    num_items_in_clusters = [0]*num_clusters
+    #get the number of items for each cluster
+    for i in range(len(labels)):
+        num_items_in_clusters[labels[i]] += 1
+    max_num = -9999
+    for i in range(num_clusters):
+        s_i = intra_cluster_dist(X, labels, clusters[i], num_items_in_clusters[i], cluster_ctr[i])
+    for j in range(num_clusters):
+        if(i != j):
+            s_j = intra_cluster_dist(X, labels, clusters[j], num_items_in_clusters[j], cluster_ctr[j])
+            m_ij = np.linalg.norm(cluster_ctr[clusters[i]]-cluster_ctr[clusters[j]])
+            r_ij = (s_i + s_j)/m_ij
+            if(r_ij > max_num):
+                max_num = r_ij
+    return max_num
+
 def cluster_func(vecs):
     # normalize embeddings
     vecs = StandardScaler().fit_transform(vecs)
@@ -288,7 +318,7 @@ def cluster_func(vecs):
 
         text_file.write("\n\n\n")
 
-    #spectral
+    # spectral
     n_clusters = 5 #2-10
     spectral = cluster.SpectralClustering(n_clusters,
                                           eigen_solver='arpack',
@@ -308,8 +338,9 @@ def cluster_func(vecs):
             str(cluster_lists[x]) + "\n\n")
     print("silhouette score: " + \
     str(metrics.silhouette_score(vecs, y_pred, metric = 'sqeuclidean')))
+    print("Davies-Bouldin Index: " + str(davies_bouldin_index(vecs, y_pred)))
 
-    #k-means
+    # k-means
     n_clusters = 5
     kmeans = cluster.KMeans(n_clusters, init='k-means++').fit(vecs)
     y_pred = kmeans.labels_.astype(np.int)
@@ -326,25 +357,9 @@ def cluster_func(vecs):
             str(cluster_lists[x]) + "\n\n")
     print("silhouette score: " + \
     str(metrics.silhouette_score(vecs, y_pred, metric = 'sqeuclidean')))
-
+    print("Davies-Bouldin Index: " + str(davies_bouldin_index(vecs, y_pred)))
 
 cluster_func(final_embeddings)
-
-# def computeDist(v1, v2):
-#   sum = 0
-#   for a,b in zip(v1,v2):
-#     sum += (a-b)**2
-#   return math.sqrt(sum)
-
-# dist_matrix = []
-# b = 0
-# for vec1 in final_embeddings[:10]:
-#   dist_vec = []
-#   for vec2 in final_embeddings:
-#     b += 1
-#     dist_vec.append(computeDist(vec1, vec2))
-#   dist_matrix.append(dist_vec)
-
 # Step 6: Visualize the embeddings.
 def plot_with_labels(low_dim_embs, labels, filename='tsne.png'):
     assert low_dim_embs.shape[0] >= len(labels), "More labels than embeddings"
